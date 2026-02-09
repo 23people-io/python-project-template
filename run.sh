2de265e92bd380948cb3906703f5286532b15775
jorgecv23 <jorge.cerda@23people.io>
2026-02-09 13:23:35 -0300

    feat: add pre-commit config

---
+++ run.sh
@@ -0,0 +1,1 @@
+uv run --env-file .env python run.py main.py
---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:23people-io/python-project-template.git
+cd python-project-template
+uv venv
+source .venv/bin/activate
+uv sync
