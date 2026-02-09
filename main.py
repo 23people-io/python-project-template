889dadfef39de6f3ed9e5e96d29a4f5e39c4b0be
jorgecv23 <jorge.cerda@23people.io>
2026-02-09 12:51:22 -0300

    feat: ruff and ty config

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     print("Hello from python-project-template!")
+    a: str = "a"
 
 
 if __name__ == "__main__":
     main()
+    try:
+        print(1 / 0)
+    except ZeroDivisionError as e:
+        print(f"Caught an exception: {e}")
