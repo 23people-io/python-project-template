a35479d5732278274bc6c6e5297797dc099b48dd
Marcelo Ampuero <marcelo@23people.io>
2026-02-11 17:30:03 -0300

    refactor: simplify endpoints to return hello world

--- routes/examples.py
+++ routes/examples.py
@@ -1,72 +1,8 @@
 from fastapi import APIRouter
 
-from examples import (
-    boolean_operators,
-    dictionaries,
-    for_while,
-    functional,
-    lists,
-    oop,
-    operators,
-    sets,
-    strings,
-    type_hints,
-    types,
-)
-
-router = APIRouter(prefix="/examples", tags=["Examples"])
+router = APIRouter(prefix="/", tags=["Examples"])
 
 
-@router.get("/boolean-operators")
-async def get_boolean_operators():
-    boolean_operators.main()
-
-
-@router.get("/for-while")
-async def get_for_while():
-    for_while.main()
-
-
-@router.get("/lists")
-async def get_lists():
-    lists.main()
-
-
-@router.get("/dictionaries")
-async def get_dictionaries():
-    dictionaries.main()
-
-
-@router.get("/functional")
-async def get_functional():
-    functional.main()
-
-
-@router.get("/oop")
-async def get_oop():
-    oop.main()
-
-
-@router.get("/operators")
-async def get_operators():
-    operators.main()
-
-
-@router.get("/sets")
-async def get_sets():
-    sets.main()
-
-
-@router.get("/type-hints")
-async def get_type_hints():
-    type_hints.main()
-
-
-@router.get("/types")
-async def get_types():
-    types.main()
-
-
-@router.get("/strings")
-async def get_strings():
-    strings.main()
+@router.get("")
+def hello():
+    return {"message": "Hello World", "status": "ok"}
