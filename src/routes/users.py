9ec1ad57b6c944090f3c51936423376a7f6d09cd
Marcelo Ampuero <marcelo@23people.io>
2026-02-11 20:40:49 -0300

    refactor: move code to service and models

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,23 +1,16 @@
-import random
-
 from fastapi import APIRouter
-from pydantic import BaseModel
+
+from models.user_model import User
+from services import users_service
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-class User(BaseModel):
-    id: int | None = None
-    name: str
-    email: str
-
-
 @router.get("/{user_id}", response_model=User)
-async def get_user(user_id: int):
-    return User(id=user_id, name="John Doe", email="john.doe@example.com")
+async def get_user(user_id: int) -> User:
+    return users_service.get_user(user_id)
 
 
 @router.post("/", response_model=User)
-async def create_user(user: User):
-    user.id = random.randint(10, 1000)  # Simulate user creation and assign an ID  # noqa: S311
-    return user
+async def create_user(user: User) -> User:
+    return users_service.create_user(user)
---
+++ src/services/users_service.py
@@ -0,0 +1,15 @@
+import random
+
+from models.user_model import User
+
+
+def get_user(user_id: int) -> User:
+
+    return User(id=user_id, name="John Doe", email="john.doe@example.com")
+
+
+def create_user(user: User) -> User:
+
+    user_id: int = random.randint(10, 1000)  # noqa: S311
+
+    return User(id=user_id, name=user.name, email=user.email)
